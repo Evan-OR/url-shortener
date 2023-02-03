3a0f1f600de6e1c989cca44e80262f2afcc9e399
18EOR <42158152+18EOR@users.noreply.github.com>
2023-02-03 23:02:57 +0000

    URL is now properly encoded and sent to database. Also Updated UI

--- frontend/scripts.js
+++ frontend/scripts.js
@@ -1,7 +1,6 @@
 const errText = document.getElementById('err');
 
 const sendRequest = async (e) => {
-  console.log('being called!');
   e.preventDefault();
   e.stopPropagation();
 
@@ -17,7 +16,7 @@ const sendRequest = async (e) => {
   }
   if (!isValidHttpUrl(url.value)) {
     errText.style.display = 'block';
-    console.log('`not valid`');
+    console.log('not valid');
     btn.disabled = false;
     return;
   }
@@ -25,11 +24,11 @@ const sendRequest = async (e) => {
   console.log('valid');
   errText.style.display = 'none';
 
-  const req = await fetch(`http://localhost:8080/create/${url.value}`, {
+  //ENDCODE URL
+  const encodedURL = encodeURIComponent(url.value);
+
+  const req = await fetch(`http://localhost:8080/create/${encodedURL}`, {
     method: 'post',
-    body: {
-      url: '',
-    },
   });
   const res = await req.json();
 
