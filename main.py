88efef8dc8db5428b2c610013ba11f4b202d7826
18EOR <42158152+18EOR@users.noreply.github.com>
2023-09-29 22:40:51 +0100

    added qr code endpoint

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-from flask import Flask, render_template, jsonify, request
+from io import BytesIO, StringIO
+from flask import Flask, render_template, jsonify, send_file, request
 from utils.DatabaseController import DatabaseController
+from utils.qr_code_generation import generate_qr_code
+from time import time
 
 # Flask Init
 app = Flask(__name__)
@@ -43,16 +46,31 @@ def display_link(code):
     # Check if link is in database
     try:
         res = dc.get_original_from_shortened(code)
+        print(res)
 
         original_url = res.get('original_url')
         shortened_url = res.get('shortened_url')
 
         full_shortened_url = request.url_root + shortened_url
+        print(full_shortened_url)
+        image_data = generate_qr_code(full_shortened_url)
 
-        return render_template('display_link.html', original_url=original_url, shortened_url=full_shortened_url), 200
+        return render_template('display_link.html', original_url=original_url, shortened_url=full_shortened_url, image_data=image_data), 200
  
-    except:
+    except Exception as e:
+        print(e)
         return render_template('404.html'), 404
+    
+@app.route('/get_qrimg')
+def get_qrimg():
+    img_buf = BytesIO()
+    start = time()
+    img = generate_qr_code(url='www.python.org')
+    end = time()
+    print(f"{(end - start) * 1000}ms")
+    img.save(img_buf)
+    img_buf.seek(0)
+    return send_file(img_buf, mimetype='image/png')
 
 @app.errorhandler(404)
 def page_not_found(e):
--- utils/qr_code_generation.py
+++ utils/qr_code_generation.py
@@ -1,17 +1,12 @@
 import qrcode
 
-def generate_qr_code(url):
-    # Generate the QR code
-    qr = qrcode.QRCode(
-        version=1,
-        error_correction=qrcode.constants.ERROR_CORRECT_L,
-        box_size=10,
-        border=4,
-    )
+def generate_qr_code(url:str):    
+    qr = qrcode.QRCode(version=1,
+                       error_correction=qrcode.constants.ERROR_CORRECT_L,
+                       box_size=1,
+                       border=4)
+
     qr.add_data(url)
     qr.make(fit=True)
-
-    # Create a PIL image from the QR code data
-    img = qr.make_image(fill_color="black", back_color="white")
-
+    img = qr.make_image()
     return img
