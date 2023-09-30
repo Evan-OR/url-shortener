fe6ee388787a9394c74d046178ce626b6e8ab25c
18EOR <42158152+18EOR@users.noreply.github.com>
2023-09-30 23:41:24 +0100

    link display now shows QR code

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-from io import BytesIO, StringIO
-from flask import Flask, render_template, jsonify, send_file, request
+import base64
+from flask import Flask, render_template, jsonify, request
 from utils.DatabaseController import DatabaseController
-from utils.qr_code_generation import generate_qr_code
-from time import time
+from utils.qr_code_generation import generate_encoded_qr
 
 # Flask Init
 app = Flask(__name__)
@@ -38,39 +37,25 @@ def create_link():
         return jsonify({"message": "Invalid URL", "error": str(e)}), 400
     
     except Exception as e:
-        print(e)
         return jsonify({"message": "error creating link", "error": str(e)}), 500
     
 @app.route("/display/<code>")
 def display_link(code):
-    # Check if link is in database
     try:
         res = dc.get_original_from_shortened(code)
-        print(res)
 
         original_url = res.get('original_url')
         shortened_url = res.get('shortened_url')
 
         full_shortened_url = request.url_root + shortened_url
-        print(full_shortened_url)
-        image_data = generate_qr_code(full_shortened_url)
 
-        return render_template('display_link.html', original_url=original_url, shortened_url=full_shortened_url, image_data=image_data), 200
+        encoded_image = generate_encoded_qr(original_url)
+
+        return render_template('display_link.html', original_url=original_url, shortened_url=full_shortened_url, encoded_image=encoded_image), 200
  
     except Exception as e:
         print(e)
         return render_template('404.html'), 404
-    
-@app.route('/get_qrimg')
-def get_qrimg():
-    img_buf = BytesIO()
-    start = time()
-    img = generate_qr_code(url='www.python.org')
-    end = time()
-    print(f"{(end - start) * 1000}ms")
-    img.save(img_buf)
-    img_buf.seek(0)
-    return send_file(img_buf, mimetype='image/png')
 
 @app.errorhandler(404)
 def page_not_found(e):
--- utils/qr_code_generation.py
+++ utils/qr_code_generation.py
@@ -1,12 +1,22 @@
+import base64
+from io import BytesIO
 import qrcode
 
-def generate_qr_code(url:str):    
-    qr = qrcode.QRCode(version=1,
-                       error_correction=qrcode.constants.ERROR_CORRECT_L,
-                       box_size=1,
-                       border=4)
-
+def generate_encoded_qr(url:str):    
+    
+    qr = qrcode.QRCode(
+        version=1,
+        error_correction=qrcode.constants.ERROR_CORRECT_L,
+        box_size=2, 
+        border=2
+    )
     qr.add_data(url)
     qr.make(fit=True)
-    img = qr.make_image()
-    return img
+
+    img_buf = BytesIO()
+    img = qr.make_image(fill_color="#535353", back_color="white") 
+    img.save(img_buf, format="PNG")
+
+    encoded_image = base64.b64encode(img_buf.getvalue()).decode('utf-8')
+
+    return encoded_image
