e47d587a1bd9f2cafae78494486651e36c13d370
Evan-OR <tehEvster@gmail.com>
2023-10-08 19:15:31 +0100

    shortened link now shows root URL

--- utils/utils.py
+++ utils/utils.py
@@ -1,10 +1,9 @@
 import os 
 from flask.wrappers import Request
 
-def get_base_url(request:Request):
+def get_base_url(request:Request, shorten:bool=False):
     env_type = os.getenv('ENV_TYPE', os.environ.get('ENV_TYPE'))
 
-    if env_type == "DEBUG":
-        return request.root_url
-    else:
-        return "https://www.lillink.net/"
+    url = request.root_url if env_type == "DEBUG" else "https://www.lillink.net/"
+
+    return url[12:] if shorten else url
