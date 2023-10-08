c6a76557077f65f1cdde3627c574a58cac64ebbd
Evan-OR <tehEvster@gmail.com>
2023-10-08 19:36:27 +0100

    formatted HTML and added comments in Python files

--- utils/utils.py
+++ utils/utils.py
@@ -2,6 +2,14 @@ import os
 from flask.wrappers import Request
 
 def get_base_url(request:Request, shorten:bool=False):
+    """
+    Construct and return a base URL.
+
+    Args:
+        request (Request): The request object containing information about the current request.
+        shorten (bool, optional): Whether to shorten the base URL by removing "https://www.". Defaults to False.
+
+    """
     env_type = os.getenv('ENV_TYPE', os.environ.get('ENV_TYPE'))
 
     url = request.root_url if env_type == "DEBUG" else "https://www.lillink.net/"
