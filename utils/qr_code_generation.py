d7bbc803ca85679e69816f18a5a91912ea5c9296
Evan-OR <tehevster@gmail.com>
2025-02-13 00:16:01 +0000

    updated QR image size

--- utils/qr_code_generation.py
+++ utils/qr_code_generation.py
@@ -2,7 +2,8 @@ import base64
 from io import BytesIO
 import qrcode
 
-def generate_encoded_qr(url:str):    
+
+def generate_encoded_qr(url: str):
     """
     Generate and encode a QR code image for a given URL.
 
@@ -16,16 +17,16 @@ def generate_encoded_qr(url:str):
     qr = qrcode.QRCode(
         version=1,
         error_correction=qrcode.constants.ERROR_CORRECT_L,
-        box_size=2, 
-        border=2
+        box_size=5,
+        border=2,
     )
     qr.add_data(url)
     qr.make(fit=True)
 
     img_buf = BytesIO()
-    img = qr.make_image(fill_color="#535353", back_color="white") 
+    img = qr.make_image(fill_color="#535353", back_color="white")
     img.save(img_buf, format="PNG")
 
-    encoded_image = base64.b64encode(img_buf.getvalue()).decode('utf-8')
+    encoded_image = base64.b64encode(img_buf.getvalue()).decode("utf-8")
 
     return encoded_image
